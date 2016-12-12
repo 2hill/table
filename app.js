3a6e5c5f2d130a7dd4035978e073b011a0fb5625
2hill <jamil.rahou@laposte.net>
2016-12-12 13:57:57 +0100

    link js to html amended

--- app.js
+++ app.js
@@ -1,57 +1,132 @@
+/*// app global variable to maintain state
+const app = {};
 
-const ready = function ()  {
-  // loading
-const people = [
-  { firstname: "Landry", surname : "O'Connel", age: 22, height: 140 },
-  { firstname: "Sharon", surname : "O'Gara", age: 52, height: 190  },
-  { firstname: "Emerson", surname : "O'Driscol", age: 32, height: 190  },
-  { firstname: "Roni", surname : "O'Sam", age: 31, height: 170  },
-];
-const tbody = document.querySelector("tbody");
+app.people = [{
+    firstname: "Landry",
+    surname: "O'Connel",
+    age: 22,
+    height: 140
+}, {
+    firstname: "Sharon",
+    surname: "O'Gara",
+    age: 52,
+    height: 190
+}, {
+    firstname: "Emerson",
+    surname: "O'Driscol",
+    age: 32,
+    height: 190
+}, {
+    firstname: "Roni",
+    surname: "O'Sam",
+    age: 31,
+    height: 170
+}];
+// app controller
+app.redraw = function() {
+    const tbody = document.querySelector("tbody");
+    // remove tbody contents
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
 
-/*for (let i = 0; i < people.length; i++) {
-  let tr = document.createElement("tr");
-  tr.innerHTML =
-  `<td>${people[i].firstname}</td><td>${people[i].firstname}</td>`
-  let td = document.createElement("td");
-  let firstname = document.createTextNode(people[i].firstname);
-  td.appendChild(firstname);
-  tr.appendChild(td);
-  tbody.appendChild(tr);*/
+    // add new contents from app.people
 
-let trContainer = document.createDocumentFragment();
+    const trContainer = document.createDocumentFragment();
 
- for(let i = 0; i < people.length; i++) {
-   let tr = document.createElement("tr");
-   tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td><td>${people[i].age}</td><td>${people[i].height}</td>`;
-     trContainer.appendChild(tr);
- }
- tbody.appendChild(trContainer);
+    for (let i = 0; i < app.people.length; i++) {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `<td>${app.people[i].surname}</td><td>${app.people[i].firstname}</td><td>${app.people[i].age}</td><td>${app.people[i].height}</td>`;
+        trContainer.appendChild(tr);
+    }
+    tbody.appendChild(trContainer);
+};
 
+    // app view
+    app.onSubmit = function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+        const newPerson = {
+            surname: formData.get("surname"), // prend un element du dom et le transforme en objet js
+            firstname: formData.get("firstname"),
+            age: formData.get("age"),
+            height: formData.get("height"),
+        };
+        app.people.push(newPerson);
+        app.redraw();
+    };
 
-
-  }
-
-  document.getElementById("sub").addEventListener("click", function(){
-      alert("Form submited");
-  });
-
-document.addEventListener("DOMContentLoaded", ready);
-
-
-
-/*let table = document.getElementById("table");
-let row = table.insertRow(1);
-
-let cell1 = row.insertCell(0);
-let Cell2 = row.insertCell(1);
-let Cell3 = row.insertCell(0);
-let Cell4 = row.insertCell(1);
-
-cell1.innerHTML = people[1];
-Cell2.innerHTML = people[1];
-Cell3.innerHTML = people[2];
-Cell4.innerHTML = people[3];
-
+// DOM bindings
+const ready = function() {
+    // Initial drawing
+    app.redraw(); // Add new contents from app.people
+    // On form submit
+    document.getElementById("sub").addEventListener("submit", app.onSubmit); // submit is better than click because you listen all types of for submition
 }
-document.addEventListener("DOMContentLoaded", ajout);*/
+document.addEventListener("DOMContentLoaded", ready);*/
+
+// app global variable to maintain state
+const app = {};
+
+// app model
+app.people = [{
+  firstname: "Landry",
+  surname: "O'Hara",
+  age: 34,
+  height: 175
+}, {
+  firstname: "Sharon",
+  surname: "O'Neil",
+  age: 51,
+  height: 177
+}, {
+  firstname: "Emerson",
+  surname: "O'Connell",
+  age: 16,
+  height: 165
+}, {
+  firstname: "Roberto",
+  surname: "O'Maley",
+  age: 30,
+  height: 199
+}];
+
+// app controller
+app.redraw = function() {
+  const tbody = document.querySelector("tbody");
+  // Remove tbody contents
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+  // Add new contents from app.people
+  const trContainer = document.createDocumentFragment();
+  for (let i = 0; i < app.people.length; i++) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${app.people[i].surname}</td><td>${app.people[i].firstname}</td><td>${app.people[i].age}</td><td>${app.people[i].height}</td>`;
+    trContainer.appendChild(tr);
+  }
+  tbody.appendChild(trContainer);
+};
+
+// app view
+app.onSubmit = function(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const newPerson = {
+    surname: formData.get("name"),
+    firstname: formData.get("surname"),
+    age: formData.get("age"),
+    height: formData.get("height")
+  };
+  app.people.push(newPerson);
+  app.redraw();
+};
+
+// DOM bindings
+const ready = function() {
+  // Initial drawing
+  app.redraw();
+  // On form submit
+  document.getElementById("form").addEventListener("submit", app.onSubmit);
+}
+document.addEventListener("DOMContentLoaded", ready);
